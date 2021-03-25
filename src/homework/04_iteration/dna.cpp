b490814514dfcfeb52b143c7d021920529d591f4
CiroJaimes <ciro.jaimesalpizar@g.austincc.edu>
2021-03-25 18:52:43 -0500

    Homework 5

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -6,15 +6,33 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
+double get_gc_content(const string &dna){
+    double content = 0.0;
+    int len = 0;
+    int i = 0;
 
-
-
+    while(dna[i] != '\0'){
+        if (dna[i] == 'G' || dna[i] == 'C'){
+            content += 1;
+        }
+        i += 1;
+        len += 1;
+    }
+    return content / len;
+}
 
 /*
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
+string get_reverse_string(string dna){
+    string rev = "";
 
+    for(int i = dna.length()-1; i>=0; i--){
+        rev += dna[i];
+    }
+    return rev;
+}
 
 
 /*
@@ -27,4 +45,23 @@ b. iterate local string variable and
 c. return string
 
 */
+string get_dna_complement(string dna){
+    string b = get_reverse_string(dna);
+    string complement = "";
 
+    for(int i = 0; i<b.length(); i++){
+        if(b[i] == 'A'){
+            complement += 'T';
+        }
+        else if(b[i] == 'T'){
+            complement += 'A';
+        }
+        else if(b[i] == 'G'){
+            complement += 'C';
+        }
+        else if(b[i] == 'C'){
+            complement += 'G';
+        }
+    }
+    return complement;
+}
--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,7 +1,8 @@
 //write include statements
-
+#include "dna.h"
+#include <iostream>
 //write using statements
-
+using namespace std;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +13,43 @@ user enters a y or Y.
 */
 int main() 
 {
+	int option;
+	string dna;
+
+	while(1){
+		cout << "Enter a DNA string: ";
+		cin >> dna;
+
+		cout << "\nSelect option" << endl;
+		cout << "1. GC Content" << endl;
+		cout << "2. DNA Complement\n" << endl;
+
+		cout << "Enter Option: ";
+		cin >> option;
+
+		if (option == 1){
+			double content = get_gc_content(dna);
+			cout << "GC content: " << content << endl;
+		}
+		else if (option == 2){
+			string complement = get_dna_complement(dna);
+			cout << "Complement: " << complement << endl;
+		}
+		else{
+			cout << "Invalid Input." << endl;
+		}
+		char ch;
+
+		cout << "\nTo continue enter (Y/y): ";
+		cin >> ch;
+
+		if(ch == 'Y' || ch == 'y'){
+			cout << "\n" << endl;
+			continue;
+		}
+		else{
+			break;
+		}
+	}
 	return 0;
 }
