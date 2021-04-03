512566478a372679aefa43d4e31b38d77a0fe6b0
CiroJaimes <ciro.jaimesalpizar@g.austincc.edu>
2021-04-02 19:21:58 -0500

    Homework 6

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,6 +1,27 @@
+#include "tic_tac_toe.h"
+#include <iostream>
+using namespace std;
 
 int main() 
 {
+	Game game;
+	string player;
+	int position;
 
+	cout<<"------------ TIC TAC TOE ------------\n";
+	cout<<"Ready to play? Player 1 enter X or O to begin: ";
+	cin>>player;
+
+	game.start_game(player);
+	game.display_board();
+
+	do
+	{
+		cout<<"Player "<<game.get_player()<<" Select a spot 1-9 to place your marker. \nEnter '10' to close the board: ";
+		cin>>position;
+		game.mark_board(position);
+		game.display_board();
+
+	}while(position != 10);
 	return 0;
 }
--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,55 @@
 //cpp
+
+#include "tic_tac_toe.h"
+#include <iostream>
+
+using namespace std;
+
+void Game::clear_board()
+{
+    for(std::size_t i=0; i < pegs.size(); ++i){
+        pegs [i] == " ";
+    }
+
+}
+
+bool Game::check_board_full()
+{
+    bool is_full = true;
+    for(std::size_t i=0; i < pegs.size(); ++i){
+        if (pegs[i] == " "){
+            is_full = false;
+            break;
+        }
+    }
+    return is_full;
+}
+
+void Game::start_game(string first_player)
+{
+    player = first_player;
+    this -> clear_board();
+}
+
+void Game::set_next_player()
+{
+    if(player == "X")
+        player = 'O';
+    else
+        player = "X";
+}
+
+void Game::mark_board(int position)
+{
+    pegs[position - 1] = this ->player;
+
+    this -> set_next_player();
+}
+
+void Game::display_board() const 
+{
+    cout<<pegs[0]<<"|"<<pegs[1]<<"|"<<pegs[2]<<"|"<<"\n";
+    cout<<pegs[3]<<"|"<<pegs[4]<<"|"<<pegs[5]<<"|"<<"\n";
+    cout<<pegs[6]<<"|"<<pegs[7]<<"|"<<pegs[8]<<"|"<<"\n";
+
+}
