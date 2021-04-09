67af6f8a85d42efedc81d65eb5afdbeeabf0f6f8
CiroJaimes <ciro.jaimesalpizar@g.austincc.edu>
2021-04-09 17:44:20 -0500

    Homework 7

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,3 +1,7 @@
+/*Play more than one game
+Allow user to quit the game
+Display winner of game*/
+
 #include "tic_tac_toe.h"
 #include <iostream>
 using namespace std;
@@ -7,21 +11,49 @@ int main()
 	Game game;
 	string player;
 	int position;
+	string again = "Y";
 
-	cout<<"------------ TIC TAC TOE ------------\n";
-	cout<<"Ready to play? Player 1 enter X or O to begin: ";
-	cin>>player;
-
-	game.start_game(player);
-	game.display_board();
-
-	do
+	while(again == "Y" || again == "y")
 	{
-		cout<<"Player "<<game.get_player()<<" Select a spot 1-9 to place your marker. \nEnter '10' to close the board: ";
-		cin>>position;
-		game.mark_board(position);
+			cout<<"------------ TIC TAC TOE ------------\n";
+			cout<<"Ready to play? Player 1 enter X or O to begin: ";
+			cin>>player;
+
+		while (player != "X" || player != "O")
+		{
+			cout<<"Player one, select X or O: ";
+			cin>>player;
+			if (player == "X" || player == "O")
+			{
+				break;
+			}
+		}
+		game.start_game(player);
 		game.display_board();
 
-	}while(position != 10);
+		while(game.game_over() == false)
+		{
+			cout<<"Player "<<game.get_player()<<", Pick a position (1-9): ";
+			cin>>position;
+			
+			while(position < 1 || position > 9)
+			{
+				cout<<"That position doesn't exist! Please try again: ";
+				cin>>position;
+			}
+			game.mark_board(position);
+			game.display_board();
+		}
+	if(game.get_winner() == "C")
+	{
+		cout<<"Cat's Scratch!";
+	}
+	
+	else
+	cout<<"Player "<<game.get_winner()<<" has WON!\n";
+	cout<<"Enter Y to play again: ";
+	cin>>again;
+	}
+	
 	return 0;
 }
